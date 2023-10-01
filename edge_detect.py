b8b8ac3af073418c93a941085be6e4944a9419ec
NRVIE <62114085+NRVIE@users.noreply.github.com>
2023-09-30 21:59:06 -0400

    Testing Q7

--- edge_detect.py
+++ edge_detect.py
@@ -48,13 +48,67 @@ def gradient(img):
     gy = convolve_3b3(img, sobel_y)
     return np.sqrt(gx ** 2 + gy ** 2)
 
+def compute_t_i(img, t):
+    sum_of_low = 0
+    low_count = 0
+    sum_of_upper = 0
+    upper_count = 0
+    for row in range(0, img.shape[0]):
+        for col in range(0, img.shape[1]):
+            curr_pix = img[row][col]
+            if (curr_pix <= t):
+                sum_of_low += curr_pix
+                low_count += 1
+            else:
+                sum_of_upper += curr_pix
+                upper_count += 1
+    ml = sum_of_low / low_count
+    mh = sum_of_upper / upper_count
+    return (ml + mh)/2
+
+def threshold(gray_img, epsilon = 0.01):
+    """Assume input gray_img is a gray scale image"""
+    img = gradient(gray_img)
+    # init threshold
+    new_img = np.zeros((img.shape[0], img.shape[1]))
+    t0 = np.sum(img) / (img.shape[0] * img.shape[1])
+    t_i = compute_t_i(img, t0)
+    while ((t_i - t0) > epsilon):
+        t0 = t_i
+        t_i = compute_t_i(img, t0)
+    for row in range(0, img.shape[0]):
+        for col in range(0, img.shape[1]):
+            if img[row][col] >= t_i:
+                new_img[row][col] = 255
+            else:
+                new_img[row][col] = 0
+    return new_img
+
 
 img = cv2.imread('image1.jpg')
-print(type(img))
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gradient_img = gradient(img_gray)
+# Q7
+img_gray_noise1 = add_gnoise(img_gray, 0.0, 0.1)
+img_gray_noise2 = add_gnoise(img_gray, 0.0, 0.3)
+# print(type(img_gray_noise1))
+# print(type(img_gray_noise2))
+# edges1 = cv2.Canny(img_gray_noise1, threshold1=75, threshold2=100)
+# edges2 = cv2.Canny(img_gray_noise2, threshold1=75, threshold2=100)
 
-plt.figure(figsize=(7, 7))
-plt.imshow(gradient_img, cmap='gray')
-plt.xticks([]), plt.yticks([])
+fig = plt.figure(figsize=(15, 15))
+ax1 = fig.add_subplot(121)
+ax2 = fig.add_subplot(122)
+
+ax1.imshow(img_gray_noise1, cmap='gray')
+ax1.title.set_text('Noise with variance=0.1'), ax1.set_xticks([]), ax1.set_yticks([])
+ax2.imshow(img_gray_noise2, cmap='gray')
+ax2.title.set_text('Noise with variance=0.3'), ax2.set_xticks([]), ax2.set_yticks([])
+
+# # Q8
+# print(type(img))
+# threshold_img = threshold(img_gray)
+#
+# plt.figure(figsize=(7, 7))
+# plt.imshow(threshold_img, cmap='gray')
+# plt.xticks([]), plt.yticks([])
