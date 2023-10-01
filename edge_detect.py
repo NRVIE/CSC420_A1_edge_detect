fbdd60a209aaadfd61c11ba7a3dd915770710c26
NRVIE <62114085+NRVIE@users.noreply.github.com>
2023-09-30 23:46:55 -0400

    Done all the tasks and tests

--- edge_detect.py
+++ edge_detect.py
@@ -6,9 +6,13 @@ from scipy import signal
 
 
 def add_gnoise(img, mean: float, sigma: float):
-    gaussian = np.random.normal(mean, sigma, (img.shape[0], img.shape[1]))
+    gaussian = np.random.normal(mean, sigma, (img.shape[0], img.shape[1])).astype(np.uint8)
     return img + gaussian
 
+def add_salt_and_pepper(img, mean: float, sigma: float):
+    salt = np.random.normal(mean, sigma, (img.shape[0], img.shape[1])).astype(np.uint8)
+    pepper = np.random.normal(mean, sigma, (img.shape[0], img.shape[1])).astype(np.uint8)
+    return img + salt - pepper
 
 def gaussian(x: int, sigma: float, mean: float = 0.0):
     return (1 / (sigma * math.sqrt(2 * math.pi))) * math.exp(
@@ -89,26 +93,70 @@ img = cv2.imread('image1.jpg')
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Q7
-img_gray_noise1 = add_gnoise(img_gray, 0.0, 0.1)
-img_gray_noise2 = add_gnoise(img_gray, 0.0, 0.3)
-# print(type(img_gray_noise1))
-# print(type(img_gray_noise2))
-# edges1 = cv2.Canny(img_gray_noise1, threshold1=75, threshold2=100)
-# edges2 = cv2.Canny(img_gray_noise2, threshold1=75, threshold2=100)
+img_gray_noise1 = add_gnoise(img_gray, 0.0, 1.0)
+img_gray_noise2 = add_gnoise(img_gray, 0.0, 6.0)
+img_gray_noise3 = add_gnoise(img_gray, 0.0, 10.0)
+img_salt_n_pepper1 = add_salt_and_pepper(img_gray, 0.0, 1.0)
+img_salt_n_pepper2 = add_salt_and_pepper(img_gray, 0.0, 6.0)
+img_salt_n_pepper3 = add_salt_and_pepper(img_gray, 0.0, 10.0)
 
+edges1 = cv2.Canny(img_gray_noise1, threshold1=75, threshold2=100)
+edges2 = cv2.Canny(img_gray_noise2, threshold1=75, threshold2=100)
+edges3 = cv2.Canny(img_gray_noise3, threshold1=75, threshold2=100)
+edges4 = cv2.Canny(img_salt_n_pepper1, threshold1=75, threshold2=100)
+edges5 = cv2.Canny(img_salt_n_pepper2, threshold1=75, threshold2=100)
+edges6 = cv2.Canny(img_salt_n_pepper3, threshold1=75, threshold2=100)
+
+fig = plt.figure(figsize=(15, 15))
+ax1 = fig.add_subplot(221)
+ax2 = fig.add_subplot(222)
+ax3 = fig.add_subplot(223)
+ax1.imshow(edges1, cmap='gray')
+ax1.title.set_text('Low Gaussian Noise (var=1)'), ax1.set_xticks([]), ax1.set_yticks([])
+ax2.imshow(edges2, cmap='gray')
+ax2.title.set_text('Medium Gaussian Noise (var=6)'), ax2.set_xticks([]), ax2.set_yticks([])
+ax3.imshow(edges3, cmap='gray')
+ax3.title.set_text('High Gaussian Noise (var=10)'), ax3.set_xticks([]), ax3.set_yticks([])
+
+fig = plt.figure(figsize=(15, 15))
+ax1 = fig.add_subplot(221)
+ax2 = fig.add_subplot(222)
+ax3 = fig.add_subplot(223)
+ax1.imshow(edges4, cmap='gray')
+ax1.title.set_text('Low Salt and Pepper Noise (var=1)'), ax1.set_xticks([]), ax1.set_yticks([])
+ax2.imshow(edges5, cmap='gray')
+ax2.title.set_text('Medium Salt and Pepper Noise (var=6)'), ax2.set_xticks([]), ax2.set_yticks([])
+ax3.imshow(edges6, cmap='gray')
+ax3.title.set_text('High Salt and Pepper Noise (var=10)'), ax3.set_xticks([]), ax3.set_yticks([])
+
+# Q8
+# Visualizing Gaussian filter
+g_filter1 = create_gfilter(3, 1)
+g_filter2 = create_gfilter(15, 3)
 fig = plt.figure(figsize=(15, 15))
 ax1 = fig.add_subplot(121)
 ax2 = fig.add_subplot(122)
+ax1.imshow(g_filter1, interpolation='none', cmap='gray')
+ax1.title.set_text('Gaussian filter (size=3 and sigma=1)'), ax1.set_xticks([]), ax1.set_yticks([])
+ax2.imshow(g_filter2, interpolation='none', cmap='gray')
+ax2.title.set_text('Gaussian filter (size=15 and sigma=3)'), ax2.set_xticks([]), ax2.set_yticks([])
+plt.show()
 
-ax1.imshow(img_gray_noise1, cmap='gray')
-ax1.title.set_text('Noise with variance=0.1'), ax1.set_xticks([]), ax1.set_yticks([])
-ax2.imshow(img_gray_noise2, cmap='gray')
-ax2.title.set_text('Noise with variance=0.3'), ax2.set_xticks([]), ax2.set_yticks([])
+# Applying Gaussian filter to an image
+g_image1 = cv2.filter2D(src=img_rgb, kernel=g_filter1, ddepth=-1)
+g_image2 = cv2.filter2D(src=img_rgb, kernel=g_filter2, ddepth=-1)
+fig = plt.figure(figsize=(15, 15))
+ax1 = fig.add_subplot(121)
+ax2 = fig.add_subplot(122)
+ax1.imshow(g_image1, interpolation='none', cmap='gray')
+ax1.title.set_text('Apply Gaussian filter (size=3 and sigma=1)'), ax1.set_xticks([]), ax1.set_yticks([])
+ax2.imshow(g_image2, interpolation='none', cmap='gray')
+ax2.title.set_text('Apply Gaussian filter (size=15 and sigma=3)'), ax2.set_xticks([]), ax2.set_yticks([])
+plt.show()
 
-# # Q8
-# print(type(img))
-# threshold_img = threshold(img_gray)
-#
-# plt.figure(figsize=(7, 7))
-# plt.imshow(threshold_img, cmap='gray')
-# plt.xticks([]), plt.yticks([])
+# Print the result of edge detection with threshold algorithm
+threshold_img = threshold(img_gray)
+plt.figure(figsize=(7, 7))
+plt.imshow(threshold_img, cmap='gray')
+plt.title('Result of edge detection with threshold algorithm')
+plt.xticks([]), plt.yticks([])
